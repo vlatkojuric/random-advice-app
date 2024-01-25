05dc7372bb1b6ea2cb923f9e666c2a982f8ac336
vlatkojuric <juricv78@gmail.com>
2024-01-25 14:36:38 +0100

    added class names,and added some logic to when show the text and the id,and when not to

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import axios from "axios";
 import Icon from "./assets/Icon";
 
 function App() {
-  const [randomAdvice, setRandomAdvice] = useState(
-    'Press the "Dice" button to hear some wisdom 🔮 '
-  );
+  const [randomAdvice, setRandomAdvice] = useState(false);
+  // 'Press the "Dice" button to hear some wisdom 🔮 '
   const [randomAdviceId, setRandomAdviceId] = useState("");
 
   function fetchAdviceData() {
@@ -17,10 +16,15 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <p>Advice #{randomAdviceId}</p>
-      <h1>{randomAdvice}</h1>
-      <button onClick={fetchAdviceData}>
+    <div className="container">
+      {!randomAdvice && (
+        <h1 className="dissapear-text">
+          Click the dice to get some random advice
+        </h1>
+      )}
+      {randomAdvice && <p className="advice-id">Advice #{randomAdviceId}</p>}
+      <h1 className="random-advice">{randomAdvice}</h1>
+      <button onClick={fetchAdviceData} className="dice-button">
         <Icon />
       </button>
     </div>
